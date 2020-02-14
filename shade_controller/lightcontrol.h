0fc1cf7beffd0c520edfa0dcfecbce38e268c65f
mrcory <33401433+mrcory@users.noreply.github.com>
2020-02-14 14:45:58 -0500

    Fix light not turnng of when slider set to 0 (hopefully)

--- shade_controller/lightcontrol.h
+++ shade_controller/lightcontrol.h
@@ -26,7 +26,7 @@ void lightControl() {
   }
 
   if (lightMode == 0) {
-    if (pwmOn == false) {
+    if (pwmOn == false || pwmBrightness == 0) {
       pwmBrightnessOut = 0;
     }
     if (pwmOn == true && pwmBrightness != pwmBrightnessOut) {
